4d803418a4d8e1911ef99859207c361b36027001
Gabriel Melo <gabriel.smelo@protonmail.com>
2022-02-28 11:43:23 -0300

    Initial project

---
+++ src/components/app.js
@@ -0,0 +1,6 @@
+import '../styles/index.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1 class="text-2xl">Hello!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
