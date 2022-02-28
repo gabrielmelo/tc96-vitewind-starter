4dc39c1e7438207697a3ab8d57a720ef73d0268f
Gabriel Melo <gabriel.smelo@protonmail.com>
2022-02-28 11:44:26 -0300

    App: fix url documentation

--- src/components/app.js
+++ src/components/app.js
@@ -2,5 +2,5 @@ import '../styles/index.css'
 
 document.querySelector('#app').innerHTML = `
   <h1 class="text-2xl">Hello!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <a href="https://github.com/gabrielmelo/tailwindcss-vitejs-starter" target="_blank">Documentation</a>
 `
